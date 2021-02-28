a87119612e2c2965330eeae6b6912aa33050ad27
kla96587 <kla96587@creighton.edu>
2021-02-28 15:26:33 -0600

    sunday work day

--- forms/ifElse/ifElse.js
+++ forms/ifElse/ifElse.js
@@ -1,11 +1,18 @@
 /*Creates the variables for first name, state, temperature, and message*/
+let name = prompt("Type your first name")
+let state = prompt ("Insert your states abbreviation (i.e. NE)")
+let temp = prompt("Insert the current temperature in degrees fahrenheit ")
 
-let firstName = ''
-let userState = ''
-let userTemperature = ''
-let userMessages = ['wear a warm coat, hat, scarf and gloves.','wear a warm coat but you won't need a hat, scarf or gloves.','wear your warmest coat, a warm hat, a scarf, and warm gloves.','wear a warm coat, hat and gloves. Maybe a scarf too.']
+if ( temp < 32 && state == 'NE')
+ console.log(`${name}, you should be wearing a warm coat, hat, scarf and gloves.`)
+ 
+else if (temp>=32 && temp < 50 && state == 'NE')
+    console.log(`${name}, you should be wearing a warm coat but you won't need a hat, scarf or gloves.`)
 
-
-if ${userTemperature} is "under 32 degrees" AND ${userState} is "NE" {
- console.log('Katelyn, [0]');
- }
+else if (temp>=32 && temp < 50 && state == 'FL')
+    console.log(`${name}, you should be wearing your warmest coat, a warm hat, a scarf, and warm gloves.`)   
+ 
+else if ((temp>=50 && temp < 70 && state == 'FL'))
+    console.log(`${name}, you should be wearing a warm coat, hat and gloves. Maybe a scarf too.`)
+ 
+    
---
+++ forms/switch1/switch1.js
@@ -0,0 +1,7 @@
+/*Creates the variables for first name, state, temperature, and message*/
+
+let firstName = "Katelyn"
+let userState = " "
+let userTemperature = " "
+let userMessages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you wont need a hat, scarf or gloves.","wear your warmest coat, a warm hat, a scarf, and warm gloves.","wear a warm coat, hat and gloves. Maybe a scarf too."]
+
