9e48a6878f07b5ae110e7d39196f868d9c093e57
kla96587 <kla96587@creighton.edu>
2021-02-28 16:01:05 -0600

    sunday2

--- forms/switch1/switch1.js
+++ forms/switch1/switch1.js
@@ -4,19 +4,22 @@ let userState = prompt ("Insert your states abbreviation (i.e. NE)")
 let userTemp = prompt("Insert the current temperature in degrees fahrenheit ")
 let usersMessages = ["wear a warm coat, hat, scarf and gloves.", "wear a warm coat but you wont need a hat, scarf or gloves.","wear your warmest coat, a warm hat, a scarf, and warm gloves.","wear a warm coat, hat and gloves. Maybe a scarf too."]
 
-let userState = 'NE' && userTemp < 32;
+/*This switch will work only if these values are true. Then the statement will output the message in the console. 
+In the console.log we use template literal to refrence the persons first name and gets the required message from the array. */
+switch(true) {
+  case (temp < 32 && state == 'NE'):
+  console.log(`${name}, ${userMessages[0]}`)
+  break;
+  
+  case (temp>=32 && temp < 50 && state == 'NE'):
+  console.log(`${name}, ${userMessages[1]}`)
+  break;
 
-switch(userState && userTemp) {
-  case ('userState = 'NE' && userTemp < 32'):
-  console.log(`${userName}, ${usersMessages[0]}`);
+  case(temp>=32 && temp < 50 && state == 'FL'):
+  console.log(`${name}, ${userMessages[2]}`)   
   break;
-  case ('userState = 'NE' && userTemp>=32 && userTemp < 50'):
-   console.log(`${userName}, ${usersMessages[1]}`);
-  break;
-   case ('userState == 'FL' && userTemp >= 32 && userTemp < 50'):
-   console.log(`${userName}, ${usersMessages[2]}`);
-  break;
-    case ('userState == 'FL' && userTemp>=50 && userTemp < 70'):
-    console.log(`${userName}, ${usersMessages[3]}`);
+  
+  case(temp>=50 && temp < 70 && state == 'FL'):
+  console.log(`${name}, ${userMessages[3]}`)
   break;
 }
